6a4d2e705f229dc9fb01ee2f69cc30e53b579037
gowthamprabhu66 <37336904+gowthamprabhu66@users.noreply.github.com>
2018-03-21 11:21:47 +0530

    Update vow.py

--- vow.py
+++ vow.py
@@ -1,6 +1,7 @@
 list=["a","e","i","o","u","A","E","I","O","U"]
 v=raw_input("")
+print (v)
 if(v in list):
-   print"the char is vowel"
+   print("the char is vowel")
 else:
-print"the char is consonant"
+     print("the char is consonant")
